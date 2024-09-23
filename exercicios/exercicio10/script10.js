1a165d20e72f77d6b54e8fe0c89659765dce13ce
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-09-23 14:07:46 -0300

    exercicio10

---
+++ exercicios/exercicio10/script10.js
@@ -0,0 +1,18 @@
+/* Estrutura while
+var c= 1
+while (c <= 4){
+ console.log(`Passo ${c}`)
+ c++
+}*/
+
+/* Estrutura Do While
+var c=1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 4)
+ */
+//Estrutura for
+for (c=1 ;c <= 4; c++) {
+    console.log(`Passo ${c}`)
+}
